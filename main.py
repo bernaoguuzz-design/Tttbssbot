a43f08924163da4c96c2186394783bc00eae8284
bernaoguuzz-design <bernaoguuzz@gmail.com>
2026-02-17 17:36:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from telegram import Bot
+import asyncio
 
 TOKEN = "8510481938:AAFNWm-ro49kiu6DFjVQ4v--hnHe012ZE-g"
 CHAT_ID = "7863143280"
@@ -9,7 +10,7 @@ bot = Bot(token=TOKEN)
 
 seen = set()
 
-def check_ttbs():
+async def check_ttbs():
     url = "https://ttbs.gtb.gov.tr/Home/BelgeSorgula"
 
     r = requests.get(url)
@@ -18,12 +19,17 @@ def check_ttbs():
     if "EMLAK" in text:
         if text not in seen:
             seen.add(text)
-            bot.send_message(chat_id=CHAT_ID,
-                             text="✅ TTBS'de yeni belge olabilir, kontrol et!")
+            await bot.send_message(
+                chat_id=CHAT_ID,
+                text="✅ TTBS'de yeni emlakçı göründü!"
+            )
 
-while True:
-    try:
-        check_ttbs()
-        time.sleep(600)  # 10 dk
-    except Exception as e:
-        print(e)
+async def main():
+    while True:
+        try:
+            await check_ttbs()
+        except Exception as e:
+            print(e)
+        await asyncio.sleep(60)
+
+asyncio.run(main())
